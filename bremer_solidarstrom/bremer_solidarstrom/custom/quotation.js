2029573fa44ae4bb1dfb769566d10d131a2ec9d4
barredterra <14891507+barredterra@users.noreply.github.com>
2023-11-28 17:07:38 +0100

    feat(Quotation): calculate item rates

--- bremer_solidarstrom/bremer_solidarstrom/custom/quotation.js
+++ bremer_solidarstrom/bremer_solidarstrom/custom/quotation.js
@@ -1,16 +1,52 @@
 frappe.ui.form.on("Quotation", {
-	global_margin (frm) {
-		const total = frm.doc.items.reduce((acc, item) => acc + item.price_list_rate * item.qty, 0);
-		const margin_factor = (total + (frm.doc.global_margin || 0) ) / total;
+	global_margin(frm) {
+		frm.trigger("update_item_rates");
+	},
+	custom_profit_margin (frm) {
+		frm.trigger("update_item_rates");
+	},
+	update_item_rates(frm) {
+		const total = frm.doc.items.reduce(
+			(acc, item) => acc + item.price_list_rate * item.qty,
+			0
+		);
+
+		const margin_factor = (total + (frm.doc.global_margin || 0)) / total;
+		const profit_factor = 1 + (frm.doc.custom_profit_margin || 0) / 100;
+		const handling_factor = 1.1;
+
 		for (const item of frm.doc.items) {
 			frappe.model.set_value(
 				item.doctype,
 				item.name,
 				"rate",
 				Math.round(
-					(item.price_list_rate * margin_factor + Number.EPSILON) * 100
+					(item.price_list_rate * handling_factor * margin_factor * profit_factor +
+						Number.EPSILON) *
+						100
 				) / 100
 			);
 		}
+	},
+	custom_fetch_global_margin(frm) {
+		const ITEM_CODE = "000.000.250";
+		const management_cost = frm.doc.items
+			.filter((item) => item.item_code === ITEM_CODE)
+			.reduce((acc, item) => acc + item.price_list_rate * item.qty, 0);
+		if (management_cost === 0) {
+			frappe.throw("No item with code 000.000.250 found");
+		}
+
+		frappe
+			.xcall(
+				"bremer_solidarstrom.bremer_solidarstrom.custom.quotation.get_global_margin",
+				{
+					to_date: frm.doc.transaction_date,
+					quotation_management_cost: management_cost,
+				}
+			)
+			.then((res) => {
+				frm.set_value("global_margin", res);
+			});
 	},
 });
---
+++ bremer_solidarstrom/bremer_solidarstrom/custom/quotation.py
@@ -0,0 +1,15 @@
+import frappe
+from frappe.utils.data import add_years, flt, get_datetime
+from bremer_solidarstrom.bremer_solidarstrom.overhead_costs import get_overhead_cost, get_management_cost
+
+
+@frappe.whitelist(methods=["POST"])
+def get_global_margin(to_date, quotation_management_cost):
+	"""Return the global margin for the year before to_date."""
+	to_date = get_datetime(to_date).date()
+	from_date = add_years(to_date, -1)
+
+	overhead_cost = get_overhead_cost(from_date, to_date)
+	total_management_cost = get_management_cost(from_date, to_date)[0]
+
+	return overhead_cost * flt(quotation_management_cost) / total_management_cost
